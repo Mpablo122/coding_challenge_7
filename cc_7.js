f93f2f665c75b5c221a26545c1c8abe4104de62a
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-15 17:49:59 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -2,8 +2,17 @@
 function calculateInvoice(subtotal, taxRate, discount){
     let total = (subtotal +(subtotal * taxRate)) - discount
     console.log(`Total invoice: $${total}`) 
-};
+}; // console log to print the statements and also creating the function
 
-calculateInvoice(100,0.08,5);
-calculateInvoice(500,0.1,20);
+calculateInvoice(100,0.08,5); // making use of the function to generate output
+calculateInvoice(500,0.1,20); // making use of the function to generate output
 
+// Task 2 - Employee Hourly Wage Calculation
+
+function calculateHourlyWage(salary, hoursPerWeek){
+    let hourly_wage = salary/(hoursPerWeek * 52)
+    console.log(`Hourly Wage: $${hourly_wage}`)
+}; // console log to print the statements and also creating the function
+
+calculateHourlyWage(52000, 40);// making use of the function to generate output
+calculateHourlyWage(75000, 35);// making use of the function to generate an output
